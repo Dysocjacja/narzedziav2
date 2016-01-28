6f1be93ed7c3d9859b868f2979eb484931c50354
Łukasz Kręski <lukaszcezarykreski@gmail.com>
2016-01-28 12:52:25 +0100

    Dodaje druga wersje plikow

--- plik.c
+++ plik.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main(){
 printf("Hello World!");
+printf("Fajnie by³oby zdaæ:
+int zdanie;
+if (zdanie==1) printf("Zdane! Czas to opic!!");
+else printf("Niezdane, czas topiæ smutki w alkoholu");
 
 return 0;
 
